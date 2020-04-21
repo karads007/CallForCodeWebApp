664f7b3d2f01e47a9bbf958ecb678fe1070a24d6
Shinoj Jose <karads007@gmail.com>
2020-04-21 19:28:18 +0100

    auto refresh

--- src/app/view-interactions/interactions.component.ts
+++ src/app/view-interactions/interactions.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Interaction } from '../models/interaction';
 import { InteractionService } from '../services/interactions.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-view-interactions',
   templateUrl: './interactions.component.html',
   styleUrls: ['./interactions.component.css']
 })
-export class InteractionsComponent implements OnInit {
+export class InteractionsComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
   interactions : Interaction[];
+  interval: any;
 
   constructor(private interactionService: InteractionService) { }
 
   ngOnInit() {
     this.getInteractions();
+    this.interval = setInterval(() => { 
+      this.getInteractions();
+     }, 5000);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    clearInterval(this.interval);
   }
 
   getInteractions(): void{
-    this.interactionService.getInteractions()
-    .subscribe(interactions => this.interactions = interactions);
+      this.interactionService.getInteractions()
+      .subscribe(interactions => this.interactions = interactions);  
   }
 }
